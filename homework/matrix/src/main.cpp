7c93e46b31241398bd3f9e48ab904abde2ed7a87
Victoria Kabaeva <tori@ktori.me>
2020-05-30 17:47:01 +0300

    feat: matrix

--- homework/matrix/src/main.cpp
+++ homework/matrix/src/main.cpp
@@ -2,7 +2,36 @@
 // Created by victoria on 30.05.20.
 //
 
+#include "matrix.hpp"
+
+#include <cassert>
+#include <iostream>
+
 int main()
 {
+	matrix<int, -1, 3> mx{};
+	assert(mx.size() == 0);
+
+	auto a = mx[0][0][0];
+	assert(a == -1);
+	assert(mx.size() == 0);
+
+	mx[100][100][100] = 314;
+	assert(mx[100][100][100] == 314);
+	assert(mx.size() == 1);
+
+//	mx[100][100][100] = -1;
+//	assert(mx[100][100][100] == -1);
+//	assert(mx.size() == 0);
+
+	for (auto i : mx)
+	{
+		size_t x, y, z;
+		int v;
+		std::tie(x, y, z, v) = i;
+
+		std::cout << x << y << z << v;
+	}
+
 	return 0;
 }
