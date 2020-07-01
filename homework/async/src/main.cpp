798c2fd1c9520371d673aee83ff78486e40acfc0
Victoria Kabaeva <tori@ktori.me>
2020-07-01 20:36:12 +0300

    feat: add async, bulkmt, bayan

---
+++ homework/async/src/main.cpp
@@ -0,0 +1,9 @@
+/*
+ * Created by victoria on 01.07.2020.
+ */
+
+void connect(){};
+
+void receive(){};
+
+void disconnect(){};
---
+++ homework/bulkmt/src/main.cpp
@@ -0,0 +1,5 @@
+/*
+ * Created by victoria on 01.07.2020.
+ */
+
+int main(int argc, const char **argv) { return 0; }
